3698333b382d16a7e3ecde2f8dbd8288541fc84d
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-01-29 18:14:57 +0300

    updated

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className='header'>
         <div className='headerLeft'>
-            <Link to="/"><img className='header__icon' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" ></img></Link>
+            <Link to="/"><img className='header__icon' src="https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png" ></img></Link>
             <Link to="/movies/popular"  style={{textDecoration:"none"}} ><span>Popular</span></Link>
             <Link to="/movies/top_rated"style={{textDecoration:"none"}} ><span>Top Rated</span></Link>
             <Link to="/movies/upcoming" style={{textDecoration:"none"}} ><span>Upcoming</span></Link>
